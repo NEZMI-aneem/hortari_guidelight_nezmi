a6220208054ac667873d8c15b5ae9d260d167db3
unknown <Administrator@SILAB-47.ASIETDomain.com>
2020-02-18 12:27:52 +0530

    merge

--- src/App.js
+++ src/App.js
@@ -1,150 +1,101 @@
+
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { render } from '@testing-library/react';
-class App extends React.Component {
+import producTile from './components/producTile';
+const productArray=[
+    {
+        "id":1,
+        "title":"lenovo",
+        "price":"Rs.30,000"
+    },
+    {"id":2,
+    "title":"Hp",
+    "price":"Rs.28,000"
 
-    constructor(props) {
+    },
+    {
+        "id":3,
+    "title":"acer",
+    "price":"Rs.32,000"
+    }
+]
+
+class App extends React.Component {
+    constructor(props){
         super(props);
-        this.state = {
-            firstState: 'hello wolrd',
-            cartCount: 0
+        this.state={
+            firstState: 'hello world',
+            cartCount: 0,
+            products:[
+                
+            ]
+
         }
     }
+        
 
 
-
-
-    componentDidMount() {
-        this.setState({
-            firstState: 'welcome to our shop'
-
-        })
+    componentDidMount(){
+        this.loadProduct( )
     }
-    addCount() {
-        const newCount = this.state.cartCount + 1;
-        this.setState({
-            cartCount: newCount
-        })
+    componentDidUpdate(prevProps,prevState)
+    {
+        if(this.state.cartCount>=10)
+        alert('count exceeded!!')
     }
 
-    render() {
-        return (
+    addCount(){
+        const newCount=this.state.cartCount+1;
+        this.setState({
+            cartCount:newCount
+        })
+    }
+    loadProduct(){
+        this.setState({
+            products:productArray
+        })
+    }y
 
+ render() {
+     const productsList =this.state.products.map((products)=>{
+         return producTile(this.addCount.bind(this),products)
+     })
+     console.log(productsList)
+    return (
+   
+        <div className="container">
+          <nav className="navbar navbar-light" style={{"backgroundColor": "red"}}>
+    }
 
-            <div className="container">
-                <nav class="navbar navbar-light" style={{ "background-color": "red" }}>
-                    {this.state.firstState}
-                    <img src="download.png" width="30" height="30" class="d-inline-block align-top" alt="" />
-
-                    <p align="right">
-                        <button type="button" className="btn btn-primary ">click {this.state.cartCount}</button>
-                    </p>
-                </nav>
-
-                <div className="row">
-                    <div className="col-3">
-                        <div className="card mb-3" style={{ "maxWidth": "540px" }}>
-                            <div className="row no-gutters">
-                                <div className="col-md-4">
-                                    <img src="download.png" className="card-img" alt="..." />
-                                </div>
-                                <div className="col-md-8">
-                                    <div className="card-body">
-                                        <h5 className="card-title">Card title</h5>
-                                        <p>
-                                            <button
-                                                className="btn btn-primary add-btn"
-                                                onClick={() => {
-                                                  
-                                                    this.addCount()
-                                                }}
-                                            >
-                                                add
-    </button>
-                                        </p>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-3">
-                        <div className="card mb-3" style={{ "maxWidth": "540px" }}>
-                            <div className="row no-gutters">
-                                <div className="col-md-4">
-                                    <img src="download.png" className="card-img" alt="..." />
-                                </div>
-                                <div className="col-md-8">
-                                    <div className="card-body">
-                                        <h5 className="card-title">Card title</h5>
-                                        <p><button type="button" className="btn btn-primary">add</button></p>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-3">
-                        <div className="card mb-3" style={{ "maxWidth": "540px" }}>
-                            <div className="row no-gutters">
-                                <div className="col-md-4">
-                                    <img src="download.png" className="card-img" alt="..." />
-                                </div>
-                                <div className="col-md-8">
-                                    <div className="card-body">
-                                        <h5 className="card-title">Card title</h5>
-                                        <p><button type="button" className="btn btn-primary">add</button></p>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-3">
-                        <div className="card mb-3" style={{ "maxWidth": "540px" }}>
-                            <div className="row no-gutters">
-                                <div className="col-md-4">
-                                    <img src="download.png" className="card-img" alt="..." />
-                                </div>
-                                <div className="col-md-8">
-                                    <div className="card-body">
-                                        <h5 className="card-title">Card title</h5>
-                                        <p><button type="button" className="btn btn-primary">add</button></p>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-3">
-                        <div className="card mb-3" style={{ "maxWidth": "540px" }}>
-                            <div className="row no-gutters">
-                                <div className="col-md-4">
-                                    <img src="download.png" className="card-img" alt="..." />
-                                </div>
-                                <div className="col-md-8">
-                                    <div className="card-body">
-                                        <h5 className="card-title">Card title</h5>
-                                        <p><button type="button" className="btn btn-primary">add</button></p>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
+    <img src="download.jpg." width="70" height="150" className="d-inline-block align-top" alt=""/>
+    {this.state.firstState}
+    
+    <p align="right">
+        <button type="button" className="btn btn-warning">Cart{this.state.cartCount}</button>
+    </p>
+    </nav>
+            <div className="row">
+                {productsList}
+                
                 </div>
-            </div>
+                
+            <p align="right">
+              <button type="button" className="btn btn-primary btn-lg">Checkout</button></p>
+    
+    </div>
+    
+    
+    
+      );
 
-
-
-
-
-
-
-
-        );
-    }
 }
+
+
+
+
+}
+
 
 export default App;
---
+++ src/components/producTile.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+export default function producTile(addCount,product){
+    return(<div className="col-3">
+    <div className="card mb-3" style={{"maxWidth": "540px"}}>
+        <div className="row no-gutters">
+            <div className="col-md-4">
+                <img src="1.jpg" className="card-img" alt="..."/>
+            </div>
+            <div className="col-md-8">
+                <div className="card-body">
+                    <h5 className="card-title">{product.title}</h5>
+                    <button
+                     className="btn btn-primary add-btn"
+                     onClick={()=>{
+                        addCount()
+                        }}
+                    >
+                    Add
+                     </button>
+                    </div>
+                
+            </div>
+        </div>
+    </div>
+</div>
+
+    )
+}
